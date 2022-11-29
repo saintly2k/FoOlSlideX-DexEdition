bca9a3824f6ff738105c1226e2b9efed002fa960
saintly2k <ninefreaks@yandex.com>
2022-11-29 13:21:42 +0100

    Permission on title publisher fix

--- themes/de_light/publisher.title.php
+++ themes/de_light/publisher.title.php
@@ -5,12 +5,12 @@ require("../../load.php");
 $id = clean(mysqli_real_escape_string($conn, $_COOKIE[config("cookie") . "_currentTitle"]));
 $title = $conn->query("SELECT * FROM `{$dbp}titles` WHERE `id`='$id' LIMIT 1")->fetch_assoc();
 
-$permission_upload = $conn->query("SELECT * FROM  `{$dbp}permissions_upload` WHERE `title_id`='$id' LIMIT 1")->fetch_assoc();
-$permission_modify = $conn->query("SELECT * FROM  `{$dbp}permissions_modify` WHERE `title_id`='$id' LIMIT 1")->fetch_assoc();
-$permission_edit = $conn->query("SELECT * FROM  `{$dbp}permissions_edit` WHERE `title_id`='$id' LIMIT 1")->fetch_assoc();
-$can_upload = $permission_upload["user_id"] == 0 || in_array($user["id"], explode(",", $permission_upload["user_id"])) ? true : false;
-$can_modify = $permission_modify["user_id"] == 0 || in_array($user["id"], explode(",", $permission_modify["user_id"])) ? true : false;
-$can_edit = $permission_edit["user_id"] == 0 || in_array($user["id"], explode(",", $permission_edit["user_id"])) ? true : false;
+$permission_upload = explode(",", $conn->query("SELECT `user_id` FROM  `{$dbp}permissions_upload` WHERE `title_id`='$id' LIMIT 1")->fetch_assoc()["user_id"]);
+$permission_modify = explode(",", $conn->query("SELECT `user_id` FROM  `{$dbp}permissions_modify` WHERE `title_id`='$id' LIMIT 1")->fetch_assoc()["user_id"]);
+$permission_edit = explode(",", $conn->query("SELECT `user_id` FROM  `{$dbp}permissions_edit` WHERE `title_id`='$id' LIMIT 1")->fetch_assoc()["user_id"]);
+$can_upload = $permission_upload == 0 || in_array($user["id"], $permission_upload) ? true : false;
+$can_modify = $permission_modify == 0 || in_array($user["id"], $permission_modify) ? true : false;
+$can_edit = $permission_edit == 0 || in_array($user["id"], $permission_edit) ? true : false;
 
 ?>
 
@@ -31,55 +31,76 @@ $can_edit = $permission_edit["user_id"] == 0 || in_array($user["id"], explode(",
         <p class="text-gray-500">Created by <a href="<?= config("url") ?>user/<?= $title["user_id"] ?>/<?= cat(getUser($title["user_id"])["name"], "username") ?>" class="text-blue-500 hover:underline"><?= getUser($title["user_id"])["name"] ?></a> on <?= $title["timestamp"] ?></p>
         <hr class="my-2">
         <div class="mb-2">
-            <?php if ($can_upload == true || $user["id"] == $title["user_id"]) { ?>
+            <?php if ($can_upload == true || $user["id"] == $title["user_id"] || $permission_upload[0] == "0") { ?>
                 <a href="<?= config("url") ?>publisher/title/<?= $title["id"] ?>/upload" target="_blank">
                     <div class="p-1 bg-green-500 text-white border border-black hover:bg-green-800 shadow-xl">Upload Chapters</div>
                 </a>
-                <p class="text-gray-500 text-left">Who can perform this action? <span class="text-gray-800"><?= $can_upload == false && $user["id"] == $title["user_id"] ? "Only You" : "Everyone" ?></span>!</p>
             <?php } else { ?>
                 <div class="p-1 bg-red-500 text-white border border-black cursor-not-allowed shadow-xl">Upload Chapters</div>
-                <p class="text-gray-500 text-left">Who can perform this action? <span class="text-gray-800">
+            <?php } ?>
+            <p class="text-gray-500 text-left">Who can perform this action?
+                <span class="text-gray-800">
+                    <?php if ($permission_upload[0] == "0") { ?>
+                        Everyone
+                    <?php } else { ?>
                         <?php $c = 1;
                         foreach ($permission_upload as $uploader) {
                             if ($c != 1) echo ", ";
-                            echo getUser($uploader, "html");
+                            $u = getUser($uploader, "html");
+                            echo '<a href="' . config("url") . 'user/' . $u["id"] . '/' . cat($u["name"]) . '" class="text-blue-500 hover:underline">' . $u["name"] . '</a>';
                             $c++;
-                        } ?></span>!</p>
-            <?php } ?>
+                        } ?>
+                    <?php } ?>
+                </span>
+            </p>
         </div>
         <div class="mb-2">
-            <?php if ($can_modify == true || $user["id"] == $title["user_id"]) { ?>
+            <?php if ($can_modify == true || $user["id"] == $title["user_id"] || $permission_modify[0] == "0") { ?>
                 <a href="<?= config("url") ?>publisher/title/<?= $title["id"] ?>/chapters" target="_blank">
                     <div class="p-1 bg-green-500 text-white border border-black hover:bg-green-800 shadow-xl">Modify Chapters</div>
                 </a>
-                <p class="text-gray-500 text-left">Who can perform this action? <span class="text-gray-800"><?= $can_modify == false && $user["id"] == $title["user_id"] ? "Only You" : "Everyone" ?></span>!</p>
             <?php } else { ?>
                 <div class="p-1 bg-red-500 text-white border border-black cursor-not-allowed shadow-xl">Modify Chapters</div>
-                <p class="text-gray-500 text-left">Who can perform this action? <span class="text-gray-800">
+            <?php } ?>
+            <p class="text-gray-500 text-left">Who can perform this action?
+                <span class="text-gray-800">
+                    <?php if ($permission_modify[0] == "0") { ?>
+                        Everyone
+                    <?php } else { ?>
                         <?php $c = 1;
                         foreach ($permission_modify as $modifier) {
                             if ($c != 1) echo ", ";
-                            echo getUser($modifier, "html");
+                            $u = getUser($modifier, "html");
+                            echo '<a href="' . config("url") . 'user/' . $u["id"] . '/' . cat($u["name"]) . '" class="text-blue-500 hover:underline">' . $u["name"] . '</a>';
                             $c++;
-                        } ?></span>!</p>
-            <?php } ?>
+                        } ?>
+                    <?php } ?>
+                </span>
+            </p>
         </div>
         <div class="mb-2">
-            <?php if ($can_edit == true || $user["id"] == $title["user_id"]) { ?>
+            <?php if ($can_edit == true || $user["id"] == $title["user_id"] || $permission_edit[0] == "0") { ?>
                 <a href="<?= config("url") ?>publisher/title/<?= $title["id"] ?>/edit" target="_blank">
                     <div class="p-1 bg-green-500 text-white border border-black hover:bg-green-800 shadow-xl">Edit Title</div>
                 </a>
-                <p class="text-gray-500 text-left">Who can perform this action? <span class="text-gray-800"><?= $can_edit == false && $user["id"] == $title["user_id"] ? "Only You" : "Everyone" ?></span>!</p>
             <?php } else { ?>
                 <div class="p-1 bg-red-500 text-white border border-black cursor-not-allowed shadow-xl">Edit Title</div>
-                <p class="text-gray-500 text-left">Who can perform this action? <span class="text-gray-800">
-                        <?php $c = 1;
-                        foreach ($permission_edit as $editors) {
-                            if ($c != 1) echo ", ";
-                            echo getUser($editors, "html");
-                            $c++;
-                        } ?></span>!</p>
             <?php } ?>
+            <p class="text-gray-500 text-left">Who can perform this action?
+                <span class="text-gray-800">
+                    <?php if ($permission_edit[0] == "0") { ?>
+                        Everyone
+                    <?php } else { ?>
+                        <?php $c = 1;
+                        foreach ($permission_edit as $editor) {
+                            if ($c != 1) echo ", ";
+                            $u = getUser($editor, "html");
+                            echo '<a href="' . config("url") . 'user/' . $u["id"] . '/' . cat($u["name"]) . '" class="text-blue-500 hover:underline">' . $u["name"] . '</a>';
+                            $c++;
+                        } ?>
+                    <?php } ?>
+                </span>
+            </p>
         </div>
         <div class="mb-2">
             <?php if ($user["id"] == $title["user_id"]) { ?>
